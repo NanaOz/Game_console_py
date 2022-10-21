02d0690fbc9f3be7a88abdfc646094db29549f15
Alina <alinavik93@gmail.com>
2022-10-21 20:56:19 +0500

    add tree, fire, whater

---
+++ main.py
@@ -0,0 +1,26 @@
+from map import Map
+import time
+import os
+
+TICK_SLEEP = 0.05
+TREE_UPDATE = 50
+FIRE_UPDATE = 100
+MAP_W, MAP_H = 20, 10
+
+tmp = Map(MAP_W, MAP_H)
+tmp.generate_forester(6, 10)
+tmp.generate_river(10)
+tmp.generate_river(30)
+tmp.print_map()
+
+tick = 1
+while True:
+    os.system("cls")
+    print("TICK", tick)
+    tmp.print_map()
+    tick += 1
+    time.sleep(TICK_SLEEP)
+    if (tick % TREE_UPDATE == 0):
+        tmp.generate_tree()
+    if(tick % FIRE_UPDATE == 0):
+        tmp.update_fires()
--- map.py
+++ map.py
@@ -1,16 +1,41 @@
-# 0 -поле
+from utils import randbool
+from utils import randcell
+from utils import rendcell2
+
+
+# 0 - поле
 # 1 - дерево
-# 2 -река
-# 3 -госпиталь
-# 4 -апгрейд
-CELL_TYPES = "🟩🌲🌊🏥🏪"
+# 2 - река
+# 3 - госпиталь
+# 4 - апгрейд
+# 5 - огонь
+
+CELL_TYPES = "🟩🌲🌊🏩🛍🔥"
 
 class Map:
+    def generate_river(self, l):
+        rc = randcell(self.w, self.h)
+        rx, ry = rc[0], rc [1]
+        self.cells[rx][ry] = 2
+        while l > 0 :
+            rc2 = rendcell2(rx, ry)
+            rx2, ry2 = rc2[0], rc2[1]
+            if (self.check_bounds(rx2, ry2)):
+                self.cells[rx2][ry2] = 2
+                rx, ry = rx2, ry2
+                l -= 1
 
-   # def generate_rivers():
+    def generate_forester(self, r, mxr):
+        for ri in range(self.h):
+            for ci in range(self.w):
+                if randbool(r, mxr):
+                    self.cells[ri][ci] = 1
 
-
-   # def generate_forester():
+    def generate_tree(self):
+        c = randcell(self.w, self.h)
+        cx, cy = c[0], c[1]
+        if (self.cells[cx][cy] == 0):
+            self.cells[cx][cy] = 1
 
     def print_map(self):
         print("⬛" * (self.w + 2))
@@ -22,6 +47,21 @@ class Map:
             print("⬛")
         print("⬛" * (self.w +2))
 
+    def add_fire(self):
+        c = randcell(self.w, self.h)
+        cx, cy = c[0], c[1]
+        if self.cells[cx][cy] == 1:
+            self.cells[cx][cy] = 5
+    
+    def update_fires(self):
+        for ri in range(self.h):
+            for ci in range(self.w):
+                cell = self.cells[ri][ci]
+                if cell == 5:
+                    self.cells[ri][ci] = 0
+        for i in range(5):
+            self.add_fire()
+
     def check_bounds(self, x, y):
         if (x < 0 or y < 0 or  x >= self.h or y >= self.w):
             return False
@@ -31,5 +71,3 @@ class Map:
         self.w = w
         self.h = h
         self.cells = [[0 for i in range(w)] for j in range(h)]
-tmp = Map(20, 10)
-tmp.print_map()
